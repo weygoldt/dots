31740fdeaf54422faaa4aaa42fcb3fcf6aaf4835
weygoldt <88969563+weygoldt@users.noreply.github.com>
2024-05-04 08:43:35 +0200

    nvim kitty changes

--- nvim/.config/nvim/lua/custom/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/custom/plugins/zen-mode.lua
@@ -2,13 +2,13 @@ return {
   'folke/zen-mode.nvim',
   opts = {
     window = {
-      backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+      backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
       -- height and width can be:
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = 120, -- width of the Zen window
-      height = 1, -- height of the Zen window
+      width = 85, -- width of the Zen window
+      height = 0.9, -- height of the Zen window
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
       options = {
@@ -32,7 +32,7 @@ return {
         -- statusline will be shown only if 'laststatus' == 3
         laststatus = 0, -- turn off the statusline in zen mode
       },
-      twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+      twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
       gitsigns = { enabled = false }, -- disables git signs
       tmux = { enabled = false }, -- disables the tmux statusline
       -- this will change the font size on kitty when in zen mode
