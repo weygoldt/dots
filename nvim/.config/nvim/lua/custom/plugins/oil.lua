449cc2cbecc90b36112ccec78b28623149b0e5d5
weygoldt <88969563+weygoldt@users.noreply.github.com>
2024-09-10 15:05:12 +0200

    removed old nvim config

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'stevearc/oil.nvim',
-  config = function()
-    require('oil').setup()
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { noremap = true, silent = true, desc = 'Toggle Oil' })
-  end,
-}
--- nvim/.config/nvim/lua/custom/plugins/rose-pine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'weygoldt/rose-pine-neovim',
-  -- 'rose-pine/neovim',
-  name = 'rose-pine',
-  priority = 100,
-  config = function()
-    vim.cmd.colorscheme 'rose-pine'
-  end,
-}
